1bd017e7ef9a051065d69b68798dd27a433e23e2
Ahmed Muhi <ae.muhi@outlook.com>
2025-11-16 08:07:51 +1300

    fix(ui): import .vue files explicitly in component barrel

--- RedDog.UI/src/components/index.js
+++ RedDog.UI/src/components/index.js
@@ -4,11 +4,11 @@ import BaseCheckbox from "./BaseCheckbox.vue";
 import BaseRadio from "./BaseRadio.vue";
 import BaseDropdown from "./BaseDropdown.vue";
 import BaseTable from "./BaseTable.vue";
-import BaseButton from "./BaseButton";
-import BaseAlert from "./BaseAlert";
-import BaseNav from "./BaseNav";
-import Modal from "./Modal";
-import CloseButton from "./CloseButton";
+import BaseButton from "./BaseButton.vue";
+import BaseAlert from "./BaseAlert.vue";
+import BaseNav from "./BaseNav.vue";
+import Modal from "./Modal.vue";
+import CloseButton from "./CloseButton.vue";
 
 import Card from "./Cards/Card.vue";
 import StatsCard from "./Cards/StatsCard.vue";
